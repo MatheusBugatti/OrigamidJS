90442a2c974bbae601c9eadf6ca96b5029e22aac
Matheus Bugatti <matheus.info@mazatarraf.com.br>
2024-06-28 10:55:18 -0300

    Declarando Variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,35 @@
+var nome = "Matheus";
+let idade = 20;
+const possuiFacul = true;
+
+console.log(nome, idade, possuiFacul);
+
+var preco = 20;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
+console.log(precoTotal);
+
+var sobrenome = 'Bugatti', cidade = 'SJRP';
+
+console.log(sobrenome, cidade);
+
+var semValor;
+
+
+var meuNome = "Matheus Bugatti",
+    minhaIdade = 19,
+    comidaPreferida
+;
+
+comidaPreferida = "lasanha";
+
+
+console.log(meuNome, minhaIdade, comidaPreferida);
+
+var variavel1,
+    variavel2,
+    variavel3,
+    variavel4,
+    variavel5
+;
