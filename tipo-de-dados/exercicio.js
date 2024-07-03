629c171a7580f919d626684eafbaed565c924be6
Matheus Bugatti <matheus.info@mazatarraf.com.br>
2024-07-03 10:23:51 -0300

    Tipos de Dados

---
+++ tipo-de-dados/exercicio.js
@@ -0,0 +1,9 @@
+var nome = "Matheus";
+
+var num = "21";
+
+var idade = 19;
+
+var sobrenome = "BUGATTI"
+
+console.log(nome + sobrenome);
---
+++ tipo-de-dados/script.js
@@ -0,0 +1,14 @@
+var nome = "MATHEUS";
+var idade = 19;
+var time = null;
+var simbolo = Symbol();
+
+console.log(typeof simbolo);
+
+var nome = "Matheus";
+var sobrenome = "Bugatti"
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+console.log(`Meu nome é ${nome} e meu sobrenome é ${sobrenome}`);
