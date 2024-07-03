4760390c74e7cbcc94e3ca823efd83d1589a0104
Matheus Bugatti <matheus.info@mazatarraf.com.br>
2024-07-03 14:41:58 -0300

    Boolean e Condicionais

--- boolean-e-condicionais/script.js
+++ boolean-e-condicionais/script.js
@@ -21,3 +21,34 @@ var x = 10
 console.log(x == '10');
 console.log(x === '10');
 console.log(x !== '10');
+
+//e &&              procura o primeiro false
+true && true //true
+true && false //false
+false && true //false
+
+//ou ||              procura o primeiro que é verdadeiro
+true || true // true
+true || false // true
+false || true //true
+'Cão' || 'Gato' //true
+(5 -5) || (5 + 5) //10
+'Cachorro' || false //Cachorro
+
+//0 é um valor falso.
+
+
+
+var corFavorita = 'amarelo';
+
+switch (corFavorita) {
+    case 'azul':
+        console.log('Olhe para o ceu');
+    break;
+    case 'amarelo':
+        console.log('Olhe o sol');
+    break
+    default:
+     console.log('Fecehe os olhos');
+    break;
+}
