eaea7fabaa3035780474ade0ccf58f62d76a499b
Matheus Bugatti <matheus.info@mazatarraf.com.br>
2024-07-03 10:48:15 -0300

    Numeros e Operadores

---
+++ numeros-e-operadores1/exercicio.js
@@ -0,0 +1,9 @@
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total); 
+
+const NanN = 'divide 5' + 10;
+console.log(NaN);
+
+let string = '200';
+let numero = 50;
+console.log(+string + numero);
---
+++ numeros-e-operadores1/script.js
@@ -0,0 +1,25 @@
+var idade = 85;
+var pi = 3.14;
+var exp = 2e10; //mais ou menos até 15 digitos
+
+var soma = '100' + 50; // 10050
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+
+testeNaN = 'isso é 10' / 2
+
+//a odem sempre importou
+var totald1 = 20 + 5 * 2 //30
+var priorizar = (20 + 5) * 2 //50
+
+var incremento = 5;
+
+console.log(incremento++);
+console.log(incremento);
+console.log(testeNaN);
+console.log(idade);
+console.log(pi);
+console.log(exp);
